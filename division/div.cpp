7d6856b529e8b8828ce844c6a0dc0364804cff09
soumyaswa <soumya.s@scaledge.io>
2023-04-17 13:12:06 +0530

    added the division file working but need modification on testbench

--- division/div.cpp
+++ division/div.cpp
@@ -6,7 +6,128 @@
 #include "right_shift.cpp"
 #include "comp.cpp"
 
-SC_MODULE(Divider) {
+
+
+SC_MODULE(divider_N) {
+    sc_in<sc_biguint<N>> dividend;
+    sc_in<sc_biguint<N>> divisor;
+    sc_out<sc_biguint<N>> Q;
+    sc_out<sc_biguint<N>> R;
+    sc_in<bool> clk, rst;
+    sc_in<sc_biguint<1>> req;
+    sc_out<sc_biguint<1>> ready, exception;
+
+    sc_signal<sc_biguint<N>> D, d, Counter, RefCounter, refD, refd, Num_cycles;
+    sc_signal<sc_biguint<N>> Dr, temp1, temp2, temp3;
+    sc_signal<sc_biguint<1>> g, e;
+
+    enum { START, COMPARE } s;
+
+    void divider_func() {
+        if (rst) {
+            s = START;
+            RefCounter = 0;
+            Counter = 0;
+            exception = 0;
+            ready = 0;
+            Num_cycles = 0;
+        } else {
+            if (s == START) {
+                if (req == 1) {
+                    if (divisor == 0) {
+                        exception = 1;
+                        ready = 1;
+                        s = START;
+                        Num_cycles = 1;
+                    } else if (divisor == 1) {
+                        exception = 0;
+                        ready = 1;
+                        s = START;
+                        Q.write(dividend.read());
+                        Num_cycles = 1;
+                    } else {
+                        s = COMPARE;
+                        refD = dividend.read();
+                        refd = divisor.read();
+                        D = dividend.read();
+                        d = divisor.read();
+                        Num_cycles = 1;
+                    }
+                } else {
+                    s = START;
+                    ready = 0;
+                    Num_cycles = 0;
+                }
+            } else if (s == COMPARE) {
+                Num_cycles = Num_cycles + 1;
+                if ((e == 1) || (g == 1)) {
+                    D = Dr;
+                    if (Counter == 0) {
+                        Counter = 1;
+                    } else {
+                        Counter = Counter << 1;
+                    }
+                } else {
+                    if (Counter != 0) {
+                        RefCounter = temp3;
+                        Counter = 0;
+                        D = temp2;
+                        refD = temp2;
+                    } else {
+                        Q.write(RefCounter);
+                        ready = 1;
+                        s = START;
+                    }
+                }
+            }
+        }
+    }
+
+    SC_CTOR(divider_N) :
+        dividend("div_dividend"),
+        divisor("div_divisor"),
+        Q("div_Q"),
+        R("div_R"),
+        req("div_req"),
+        ready("div_ready"),
+        exception("div_exception"),
+        rst("div_rst"),
+        clk("div_clk")
+    {
+        GE* ge_i = new GE("ge_i");
+        ge_i->A(D);
+        ge_i->B(d);
+        ge_i->G(g);
+        ge_i->E(e);
+
+        SC_METHOD(divider_func);
+        sensitive << clk.pos() << rst.pos();
+
+        Mul* mul_i = new Mul("mul_i");
+        mul_i->A(refd);
+        mul_i->B(Counter);
+        mul_i->O(temp1);
+
+        Sub* sub_i = new Sub("sub_i");
+        sub_i->A(refD);
+        sub_i->B(temp1);
+        sub_i->O(temp2);
+
+        Add* add_i = new Add("add_i");
+        add_i->A(RefCounter);
+        add_i->B(Counter);
+        add_i->O(temp3);
+
+        Rshift* rshift_i= new Rshift("rshift_i");
+        rshift_i->i(D);
+	rshift_i->o(Dr);
+    }
+};
+
+
+
+
+/*SC_MODULE(Divider) {
   sc_in<bool> clk, rstn, req;
   sc_in<sc_biguint<N>> Dividend, Divisor;
   sc_out<sc_biguint<N>> Q, R;
@@ -122,5 +243,5 @@ SC_MODULE(Divider) {
     ge.G(g);
     ge.E(e);
   }
-};
+};*/
 
--- division/div_tb.cpp
+++ division/div_tb.cpp
@@ -1,5 +1,6 @@
 #include <systemc.h>
-#include "new_div.cpp"
+//#include "new_div.cpp"
+#include "div.cpp"
 
 SC_MODULE(divider_N_tb) {
     sc_clock clk;
@@ -13,11 +14,17 @@ SC_MODULE(divider_N_tb) {
     sc_signal<sc_biguint<1>> exception;
 
     divider_N* dut;
+     // Open trace file
+        sc_trace_file* tf = sc_create_vcd_trace_file("trace");
 
-    void run_test() {
+    /*void run_test() {
         // Initialize signals
-        dividend = 123;
-        divisor = 7;
+       // dividend = 15;
+       // divisor = 2;
+        srand(time(NULL)); // Seed the random number generator
+        dividend = rand() % (1 << N); // Generate a random N-bit number
+        divisor = rand() % (1 << N); // Generate a random N-bit number
+        
         req = 0;
         rst = 1;
         wait(1, SC_NS);
@@ -30,8 +37,34 @@ SC_MODULE(divider_N_tb) {
             wait(1, SC_NS);
         }
         // Check the result
-        assert(Q.read() == 17);
-    }
+        //assert(Q.read() == 1);
+	cout<< "Current simulation time: " << sc_time_stamp() << " reset =  " << rst.read()<< " dividend =  " << dividend.read() << " divisor =  " << divisor.read() << " Q =  "<< Q.read() <<" R="<< R.read()<< endl;
+    }*/
+ void run_test() {
+        
+        for (int i = 0; i < 10; i++) {
+            // Initialize signals with random values
+            dividend = rand();
+            divisor = rand();
+            req = 0;
+            rst = rand() % 2; // random reset
+            wait(1, SC_NS);
+            rst = 0;
+            wait(1, SC_NS);
+            req = rand() % 2; // random req
+            wait(1, SC_NS);
+            // Wait until the divider is done
+            while (ready.read() == 0) {
+                wait(1, SC_NS);
+            }
+            // Check the result
+            //assert(Q.read() == 1);
+            cout << "Current simulation time: " << sc_time_stamp() << " reset = " << rst.read() << " req = " << req.read()<< " dividend = " << dividend.read() << " divisor = " << divisor.read() << " Q = " << Q.read() << " R = " << R.read() << endl;
+            wait(10, SC_NS);
+	    // Dump signals to trace file
+                }
+ }
+    
 
     SC_CTOR(divider_N_tb)
         : clk("clk", 1, SC_NS)
@@ -47,17 +80,33 @@ SC_MODULE(divider_N_tb) {
         dut->rst(rst);
         dut->clk(clk);
 
+	sc_trace(tf, clk, "clk");
+            sc_trace(tf, rst, "rst");
+            sc_trace(tf, dividend, "dividend");
+            sc_trace(tf, divisor, "divisor");
+            sc_trace(tf, req, "req");
+            sc_trace(tf, Q, "Q");
+            sc_trace(tf, R, "R");
+            sc_trace(tf, ready, "ready");
+            sc_trace(tf, exception, "exception");
+       
+
         SC_THREAD(run_test);
     }
 
     ~divider_N_tb() {
-        delete dut;
+        //delete dut;
+
+        // Close trace file
+        sc_close_vcd_trace_file(tf);
+
     }
 };
 
 int sc_main(int argc, char* argv[]) {
     divider_N_tb tb("tb");
     sc_start(100,SC_NS);
+    //sc_stop();
     return 0;
 }
 
